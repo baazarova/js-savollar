6bea6043c7a87266c927d85c0ffc71b1c2d9ed70
baazarova <iskandarovaogiloy@gmail.com>
2022-10-14 14:20:38 +0500

    jssavollar adding

---
+++ index.js
@@ -0,0 +1,31 @@
+let btnFirst1 = document.querySelector(".btn1")
+let btnFirst2 = document.querySelector(".btn2")
+let btnFirst3 = document.querySelector(".btn3")
+
+btnFirst1.addEventListener("click" , function() {
+    alert("HTML") 
+    console.log(prompt("HTML nechanchi yil o'ylab topilgan?"))
+    console.log(prompt("Necha xil teglar mavjud?"))
+    console.log(prompt("Sarlavhalar uchun qaysi teg ishlatiladi?"))
+    console.log(prompt("Rasm qo'yish uchun qaysi tegdan foydalaniladi?"))
+    console.log(prompt("Html dasturlash tiliga kiradimi?"))
+
+})
+btnFirst2.addEventListener("click" , function() {
+    alert("CSS") 
+    console.log(prompt("CSS ning asosiy vazifasi?"))
+    console.log(prompt("CSS yozishning necha xil usuli mavjud?"))
+    console.log(prompt("CSS da qanday usullarda rangni belgilash mumkin?"))
+    console.log(prompt("CSSning qaysi avlodi hozirgi kunda ishlatiladi?"))
+    console.log(prompt("Necha xil display holatlari mavjud?"))
+
+})
+btnFirst3.addEventListener("click" , function() {
+    alert("Java-Script") 
+    console.log(prompt("CSS ning asosiy vazifasi?"))
+    console.log(prompt("CSS yozishning necha xil usuli mavjud?"))
+    console.log(prompt("CSS da qanday usullarda rangni belgilash mumkin?"))
+    console.log(prompt("CSSning qaysi avlodi hozirgi kunda ishlatiladi?"))
+    console.log(prompt("Necha xil display holatlari mavjud?"))
+
+})
